ecfcfbaa3d9f84179a349eab034b8deb8fbdf3d3
Sterre <xsterrestorm@outlook.com>
2024-12-01 16:03:08 +0100

    extra zooi

--- Day01.py
+++ Day01.py
@@ -1,50 +1,54 @@
 # "ja laten we alles netjes een eigen functie geven dat is vast handig"
 
+import time
+
 def parse_input(filename):
     with open(filename) as inp:
-        list1 = []
-        list2 = []
+        left_list = []
+        right_list = []
         for line in inp:
             pos1, pos2 = line.split()
-            list1.append(int(pos1))
-            list2.append(int(pos2))
-        return list1, list2
+            left_list.append(int(pos1))
+            right_list.append(int(pos2))
+        return left_list, right_list
 
 
-def sort_list(list1, list2):
-    list1.sort()
-    list2.sort()
-    return list1, list2
+def sort_list(left_list, right_list):
+    left_list.sort()
+    right_list.sort()
+    return left_list, right_list
 
 
-def calculate_distance(list1, list2):
+def calculate_distance(left_list, right_list):
     distance = 0
-    for i, value in enumerate(list1):
-        distance += abs(value - list2[i])
+    for i, value in enumerate(left_list):
+        distance += abs(value - right_list[i])
     return distance
 
 
-def find_similarity_score(list1, list2):
+def find_similarity_score(left_list, right_list):
     similarity_score = 0
-    for value in list2:
-        similarity_score += value * list1.count(value)
+    for value in right_list:
+        similarity_score += value * left_list.count(value)
     return similarity_score
 
 def main(filename):
+    start_time = time.time_ns()
     if filename.find("short") > 0:
         version = "test input"
     else:
         version = "actual input"
 
     # pt 1
-    list1, list2 = parse_input(filename)
-    list1, list2 = sort_list(list1, list2)
-    distance = calculate_distance(list1, list2)
+    left_list, right_list = parse_input(filename)
+    left_list, right_list = sort_list(left_list, right_list)
+    distance = calculate_distance(left_list, right_list)
     print(f"distance {version}: {distance}")
+    print("--- %s ms ---" % ((time.time_ns() - start_time) / 1000000))
     # pt 2
-    similarity_score = find_similarity_score(list1, list2)
+    similarity_score = find_similarity_score(left_list, right_list)
     print(f"similarity score {version}: {similarity_score}")
-
+    print("--- %s ms ---" % ((time.time_ns() - start_time) / 1000000))
 
 main("input/day01_short.txt")
 main("input/day01.txt")
