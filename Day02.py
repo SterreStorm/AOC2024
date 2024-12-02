d94f6dc655c9c01ac88fa4e05baf28e29784bcc4
Sterre <xsterrestorm@outlook.com>
2024-12-02 11:03:09 +0100

    Day 02 Part 1 + 2 with a merged function

--- Day02.py
+++ Day02.py
@@ -1,3 +1,4 @@
+import copy
 import time
 
 
@@ -47,12 +48,31 @@ def report_safe_dampener(report):
     return safe
 
 
+def report_safe_combined(report, dampener=False):
+    safe = False
+    report_copy = report.copy()
+    decreasing = (report[0] - report[-1] > 0)
+    for i in range(len(report) - 1):
+        delta = report[i] - report[i + 1]
+        delta_decreasing = delta > 0
+        if 3 < abs(delta) or abs(delta) < 1 or delta_decreasing != decreasing:
+            if not dampener:
+                return safe
+            report_copy.pop(i + 1)
+            report.pop(i)
+            return report_safe_combined(report) or report_safe_combined(report_copy)
+    safe = True
+    return safe
+
+
+
 def main(filename):
     # variables
     start_time = time.time_ns()
     version = find_version(filename)
     valid_reports = 0
     valid_with_dampener = 0
+    valid_with_dampener_combined = 0
 
     # parse input
     parsed_input = parse_input(filename)
@@ -61,14 +81,20 @@ def main(filename):
     for report in parsed_input:
         if report_safe(report):
             valid_reports += 1
+
     # pt 2
         if report_safe_dampener(report):
             valid_with_dampener += 1
 
+    #pt 2 as originally intended
+        if report_safe_combined(report, True):
+            valid_with_dampener_combined += 1
+
     print(f"valid reports {version}: {valid_reports}")
-    print("--- %s ms ---" % ((time.time_ns() - start_time) / 1000000))
 
     print(f"valid with dampener {version}: {valid_with_dampener}")
+
+    print(f"valid with dampener as initially intended {version}: {valid_with_dampener_combined}")
     print("--- %s ms ---" % ((time.time_ns() - start_time) / 1000000))
 
 
